fdea91d54d370aaad5c4b24635c5fac5ed0f8dee
Bilal Mehdi <smehdi@advent.com>
2017-01-09 16:10:20 -0800

    Added logging

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,20 +1,25 @@
 package bootwildfly;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloWildFlyController.class);
 
     @RequestMapping("hello")
     public String sayHello(){
+        log.info("Hello API Invoked");
         return ("Hello, SpringBoot on Wildfly");
     }
 
     @RequestMapping("killSwitch")
     public void killSwitch()
     {
+        log.info("Kill Switch Invoked");
         System.exit(0);
     }
 }
