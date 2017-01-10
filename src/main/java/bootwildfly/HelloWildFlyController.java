0346a105da795f2298575c9f7715a3b5d9f4ce56
bilalmehdi <smehdi@smehdi.advent.com>
2017-01-09 21:57:44 -0800

    Added get datetime api

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class HelloWildFlyController {
 
@@ -15,6 +19,16 @@ public class HelloWildFlyController {
         log.info("Hello API Invoked");
         return ("Hello, SpringBoot on Wildfly");
     }
+
+    @RequestMapping("GetDateTime")
+    public String getDateTime(){
+        log.info("GetDateTime Invoked");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return (dateFormat.format(date)); //2016/11/16 12:08:43
+
+    }
+
 
     @RequestMapping("killSwitch")
     public void killSwitch()
